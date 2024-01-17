3a50f6f5ae67f79bfe639352d1cc39d6955c28a8
KaVeer Panthi <30440787+kvr24611@users.noreply.github.com>
2024-01-17 10:56:22 +0545

    Update jenkins-build.sh

--- docker/docker-examples/jenkins-build.sh
+++ docker/docker-examples/jenkins-build.sh
@@ -7,6 +7,6 @@ docker image rm node_image:$(cat ../image_vers)
 cd docker/docker-examples
 docker image build -t node_image:$BUILD_NUMBER .
 
-cd /var/lib/jenkins/build-docker-image
+cd /var/lib/jenkins/workspace/build-docker-image
 echo $BUILD_NUMBER > image_vers
 echo $BUILD_NUMBER > ../image_vers

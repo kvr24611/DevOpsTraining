41825a2fee81e4c075abdf93891a81c8214514fb
KaVeer Panthi <30440787+kvr24611@users.noreply.github.com>
2024-01-17 15:21:30 +0545

    Update test.sh

--- docker/docker-examples/test.sh
+++ docker/docker-examples/test.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/bash
 
-imageName=$(docker images | awk -F ' ' '{print $3}' | tail -n +2)
-containerName=$(docker container ps | awk -F ' ' '{print $1}'| tail -n +2)
+imageName=node_image
+containerName=weather_app_container
 jenkinsJobName="build-docker-image"
 
-docker container stop $containerName &> /dev/null
-docker container rm $containerName &> /dev/null
-docker image rm $imageName &> /dev/null
+
+
+
+docker container stop $(docker container ps | awk -F ' ' '{print $1}'| tail -n +2) &> /dev/null
+docker container rm $(docker container ps | awk -F ' ' '{print $1}'| tail -n +2) &> /dev/null
+docker image rm $(docker images | awk -F ' ' '{print $3}' | tail -n +2) &> /dev/null
 
 cd docker/docker-examples
 docker image build -t $imageName:$BUILD_NUMBER .

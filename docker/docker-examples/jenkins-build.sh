c278d0bdcb5f918f0a2ef94ee3bd2101ff2ff079
KaVeer Panthi <30440787+kvr24611@users.noreply.github.com>
2024-01-17 10:49:49 +0545

    Update jenkins-build.sh

--- docker/docker-examples/jenkins-build.sh
+++ docker/docker-examples/jenkins-build.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/bash
 
+docker container stop weather_app
+docker container rm weather_app
+docker image rm node_image:$(cat ../image_vers)
+
 cd docker/docker-examples
 docker image build -t node_image:$BUILD_NUMBER .
+
+echo $BUILD_NUMBER > image_vers
+echo $BUILD_NUMBER > ../image_vers

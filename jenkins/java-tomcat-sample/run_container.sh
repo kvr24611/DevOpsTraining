bbf98c976c998ccde0793ddc7ce30b2704e0e257
KaVeer Panthi <30440787+kvr24611@users.noreply.github.com>
2024-01-21 17:57:35 +0545

    Update run_container.sh

--- jenkins/java-tomcat-sample/run_container.sh
+++ jenkins/java-tomcat-sample/run_container.sh
@@ -2,4 +2,5 @@
 
 image_version=$(cat ../image_version)
 containerName="tomcat_container"
-docker container run -d --name $containerName -p 8083:8080 node_image:$image_version
+imageName=tomcat_local
+docker container run -d --name $containerName -p 8083:8080 $imageName:$image_version
